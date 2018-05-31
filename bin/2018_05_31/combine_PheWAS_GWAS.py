a4d9d755e70d3c9cd4eb393956d09b01cae3890a
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-05-31 10:54:40 -0500

    Converted to juypter notebook

---
+++ bin/2018_05_31/combine_PheWAS_GWAS.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+# Import pandas for data combining
+import pandas as pd
+
+# Open the GWAS catalog as a dataframe.
+GWAS = pd.read_table("../../data/2018_05_29/Zhang et al PTB 10000/pre_top10000.tab")
+
+# Get column of GWAS containing only the SNPs and convert to dataframe
+GWAS_SNPS = pd.DataFrame(GWAS['snp'])
+
+# Open the PheWAS catalog as a dataframe.
+PheWAS = pd.read_csv('../../data/2018_05_29/PheWAS Catalog/phewas-catalog.csv',)
+
+
+# In[3]:
+
+
+# Use inner join to combine the GWAS file with PheWAS file
+combined = pd.merge(GWAS_SNPS, PheWAS, how='inner', on='snp')
+
+# Write the result to a csv file
+combined.to_csv("../../results/2018_05_31/Combined_GWAS_PheWAS.csv", index = False)
+
