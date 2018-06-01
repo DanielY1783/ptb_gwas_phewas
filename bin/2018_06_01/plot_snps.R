6b76bee9e2e7f2a57b5b85d95cf92cf170f35ef3
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-06-01 15:21:48 -0500

    Implemented reading in data

--- bin/2018_06_01/plot_snps.R
+++ bin/2018_06_01/plot_snps.R
@@ -1,14 +1,20 @@
-# Import ggplot2 for graphs and dplyr for dataframes.
+# Import ggplot2 for graphs, dplyr for dataframes, and stringr for string manipulation.
 library(ggplot2)
 library(dplyr, warn.conflicts = FALSE)
+library(stringr)
 
 # Read in all the SNPs
 snps_df <- read.table("../../data/2018_06_01_snps_for_R/set_output.csv", sep = '\t')
 
+# Convert to vector for iteration
+snps_vec <- as.vector(t(snps_df))
+
 # Iterate over all the SNPs and open each csv file.
-for (i in 1:ncol(snps_df)){
-  cur_snp_str <- toString(snps_df[i])
-  #cur_snp_df <- read.table(paste("../../data/2018_06_01_snps_for_R/snps/", cur_snp_str, ".csv"))
-  print(snps_df[i])
+for (i in snps_vec){
+  cur_snp_str = paste("../../data/2018_06_01_snps_for_R/snps/", i, ".csv", sep = "")
+  cur_snp_df <- read.table(cur_snp_str, sep = '\t')
+  if(i ==1){
+    print(cur_snp_df)
+  }
 }
 
