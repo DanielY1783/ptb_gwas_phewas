1b0042cbc7a5e7dcdc2e333a51918af397f1dfd4
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-06-01 17:00:59 -0500

    Worked on scatter plot

--- bin/2018_06_01/plot_snps.R
+++ bin/2018_06_01/plot_snps.R
@@ -9,16 +9,24 @@ snps_df <- read.table("../../data/2018_06_01_snps_for_R/set_output.csv", sep = '
 # Convert to vector for iteration
 snps_vec <- as.vector(t(snps_df))
 
-print(snps_vec)
-
 # Iterate over all the SNPs in both the GWAS and PheWAS
 for (i in snps_vec){
   # Open the files for each SNP.
   cur_snp_str <- paste("../../data/2018_06_01_snps_for_R/snps/", i, ".csv", sep = "")
   cur_snp_df <- read.csv(cur_snp_str, sep = '\t')
-  # Get column of the phewas_string
-  # phewas_strings_vec = cur_snp_df[,c("phewas_string")]
-  # print(phewas_strings_vec)
+  
+  # Get column of the phewas_string as vector
+  phewas_strings_df <- cur_snp_df[,c("phewas_string")]
+  phewas_strings_vec <- as.vector(t(phewas_strings_df))
+  print(length(phewas_strings_vec))
+  
+  # Get column of the p_values as a vector
+  p_values_df <- cur_snp_df[,c("p")]
+  p_values_vec <- as.vector(t(p_values_df))
+  
+  # Create a plot of phewas_strings vs p_values
+  qplot(phewas_strings_df, p_values_df)
+  pdf_file_name <- paste(i, ".pdf", sep = "")
+  dev.copy2pdf(file = pdf_file_name)
 }
-
 
